3290db967d7f185a76b174a29303666f8c144824
Areeshaywaheed <128454186+Areeshaywaheed@users.noreply.github.com>
2023-08-16 22:08:08 +0500

    Add files via upload

---
+++ word scramble game/js/words.js
@@ -0,0 +1,154 @@
+let words = [
+    {
+        word: "addition",
+        hint: "The process of adding numbers"
+    },
+    {
+        word: "meeting",
+        hint: "Event in which people come together"
+    },
+    {
+        word: "number",
+        hint: "Math symbol used for counting"
+    },
+    {
+        word: "exchange",
+        hint: "The act of trading"
+    },
+    {
+        word: "tree",
+        hint: "In which leaves, vegetable and fruits are present"
+    },
+    {
+        word: "canvas",
+        hint: "Piece of fabric for oil painting"
+    },
+    {
+        word: "garden",
+        hint: "Space for planting flower and plant"
+    },
+    {
+        word: "position",
+        hint: "Location of someone or something"
+    },
+    {
+        word: "feather",
+        hint: "Hair like outer covering of bird"
+    },
+    {
+        word: "comfort",
+        hint: "A pleasant feeling of relaxation"
+    },
+    {
+        word: "tongue",
+        hint: "The muscular organ of mouth"
+    },
+    {
+        word: "expansion",
+        hint: "The process of increase or grow"
+    },
+    {
+        word: "country",
+        hint: "A politically identified region"
+    },
+    {
+        word: "group",
+        hint: "A number of objects or persons"
+    },
+    {
+        word: "taste",
+        hint: "Ability of tongue to detect flavour"
+    },
+    {
+        word: "store",
+        hint: "Large shop where goods are traded"
+    },
+    {
+        word: "field",
+        hint: "Area of land for farming activities"
+    },
+    {
+        word: "friend",
+        hint: "Person other than a family member"
+    },
+    {
+        word: "pocket",
+        hint: "A bag for carrying small items"
+    },
+    {
+        word: "brain",
+        hint: "Source of knowledge"
+    },
+    {
+        word: "mobile",
+        hint: "In which all apps are present"
+    },
+    {
+        word: "quaid-e-azam",
+        hint: "The founder of pakistan"
+    },
+    {
+        word: "clock",
+        hint: "A things which tells the time"
+    },
+    {
+        word: "teacher",
+        hint: "A person who teaches us"
+    },
+    {
+        word: "parents",
+        hint: "Our first love"
+    },
+    {
+        word: "money",
+        hint: "Source of income"
+    },
+    {
+        word: "needle",
+        hint: "A thin and sharp metal pin"
+    },
+    {
+        word: "rainbow",
+        hint: "After the rain it is appeared in the sky"
+    },
+    {
+        word: "expert",
+        hint: "Person with extensive knowledge"
+    },
+    {
+        word: "sun",
+        hint: "It gives heat and light"
+    },
+    {
+        word: "statement",
+        hint: "A declaration of something"
+    },
+    {
+        word: "pen",
+        hint: "For writing something"
+    },
+    {
+        word: "second",
+        hint: "One-sixtieth of a minute"
+    },
+    {
+        word: "smit",
+        hint: "The center where poor and needy people are helped"
+    },
+    {
+        word: "library",
+        hint: "Place containing collection of books"
+    },
+    {
+        word: "water",
+        hint: "It is drink for surviving a life"
+    },
+    {
+        word: "mango",
+        hint: "The king of fruits"
+    },
+    {
+        word: "urdu",
+        hint: "The national language"
+    },
+]
